1592db91dba571b9d945b804c3a94536189e2455
linelotus <189575054+linelotus@users.noreply.github.com>
2025-07-28 00:46:13 +0000

    timer

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom/client'
+import PropTypes from 'prop-types';
 
 //Bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -11,8 +12,51 @@ import '../styles/index.css'
 // components
 import Home from './components/Home';
 
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
+// function SimpleCounter(props){
+
+function SimpleCounter() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCount(c => c + 1);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+  
+  const d1 = Math.floor(count/1000) %10;
+  const d2 = Math.floor(count/100) %10;
+  const d3 = Math.floor(count/10) %10;
+  const d4 = Math.floor(count %10);
+
+  return (<div className="bigCounter">
+
+          <div className="timer"> <i class="fa-solid fa-alarm-clock"></i> </div>
+          <div className="d1">{d1}</div>
+          <div className="d2">{d2}</div>
+          <div className="d3">{d3}</div>
+          <div className="d4">{d4}</div>
+  </div>);
+}
+
+SimpleCounter.propTypes ={
+  //digitOne: PropTypes.number,
+   //digitTwo: PropTypes.number,
+  // digitThree: PropTypes.number,
+  // digitFour: PropTypes.number,
+};
+
+//console.log(d1, d2, d3, d4);
+  
+  
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <SimpleCounter /> 
+    
     <Home/>
   </React.StrictMode>,
-)
+); 
